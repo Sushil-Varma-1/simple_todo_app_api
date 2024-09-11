74aa8d8eaeae138e7bf4db9dadb65d57272c871a
sushil varma <svarmacompdse2021@bharatedu.co.in>
2024-09-11 12:58:49 +0530

    collected static files

--- todo/views.py
+++ todo/views.py
@@ -31,7 +31,7 @@ class TodoListCreateAPIView(APIView):
 
     def get(self, request):
 
-        todos = Todo.objects.all()
+        todos = Todo.objects.all().order_by('-id')
         serializer = TodoSerializer(todos, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
