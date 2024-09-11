8141171ad04710ddf50232fa6a3b96324e1ab2ff
sushil varma <svarmacompdse2021@bharatedu.co.in>
2024-09-11 12:29:59 +0530

    Added CORS settings

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'todo'
 ]
 
@@ -49,9 +50,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500/index.html',
+]
 
 TEMPLATES = [
     {
